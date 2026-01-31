d3bf5f5c743386611add04e0c006b47f889c68f5
DojaChok <makato1259@gmail.com>
2026-01-31 13:55:21 +0700

    Operators

---
+++ python2.py
@@ -0,0 +1,54 @@
+"""
+#
+# Part: python operations
+# Arithmetic operations
+#
+"""
+x = 4 + 5
+y = 10 - 2
+z = 3 * 7
+a = 20 / 4 
+b = 10 % 3
+c = 2 ** 3
+
+"""
+"""
+
+x = 5
+x = x + 3
+x += 3
+
+"""
+#
+# Part: python operations
+# Arithmetic operations
+#
+"""
+
+a = 5
+b = 10
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+
+"""
+#
+# Part: python operations
+# Arithmetic operations
+#
+"""
+
+x = true
+y = false
+print(x and y)
+print(x or y)
+print(not x)
+print(x & y)
+print(x | y)
+print(~x)
+a = 5
+b = 10
+print(a < 10 and b > 5)
