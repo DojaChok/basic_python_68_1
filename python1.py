87265c25f9aa3e4d24c1d980fd35176e7141d877
DojaChok <makato1259@gmail.com>
2026-01-31 13:16:59 +0700

    Setup project

---
+++ python1.py
@@ -0,0 +1,26 @@
+"""
+"""
+
+print("Hello World!")
+
+"""
+"""
+x = str(5)
+y = int("5")
+z = float("5.0")
+print(type(x), type(y), type(z))
+
+myvar = 100
+my_var = 200
+_my_var = 300
+myVar = 400
+MYVAR = 500
+#my-var = 600  # This will cause a syntax error
+#1myvar = 700  # This will cause a syntax error
+
+#camelCase
+myVariableName = 900
+#PascalCase
+MyVariableName = 1000
+#snake_case
+my_variable_name = 1100
