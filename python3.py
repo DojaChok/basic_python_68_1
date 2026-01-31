2d9db6d8abf8802f53bd478b15c59e655001b436
DojaChok <makato1259@gmail.com>
2026-01-31 14:12:03 +0700

    Collection of Python

---
+++ python3.py
@@ -0,0 +1,48 @@
+"""
+#
+# Part: Collection of python
+# Lists, tuples, sets, Dictionaries
+#
+"""
+
+this_list = ["apple", "banana", "cherry", "date", "elderberry", "melon", "kiwi", "orange"]
+print(this_list)
+print(this_list[1])
+print(type(this_list))
+print(len(this_list))
+
+"""
+#
+# Tuples
+#
+"""
+
+this_tuple = ("apple", "banana", "cherry", "date", "elderberry", "melon", "kiwi", "orange")
+print(this_tuple)
+print(this_tuple[1])
+print(type(this_tuple))
+print(len(this_tuple))
+
+"""
+#
+# sets
+#
+"""
+this_set = {"apple", "banana", "cherry", "date", "elderberry", "melon", "kiwi", "orange"}
+print(this_set)
+print(type(this_set))
+print(len(this_set))
+
+"""
+#
+# Dictionaries
+#
+"""
+this_dict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964}
+print(this_dict)
+print(this_dict["model"])
+print(type(this_dict))
+print(len(this_dict))
