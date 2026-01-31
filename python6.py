7e788cfb410ae4a30f9eb47c293cc4c45301c60a
DojaChok <makato1259@gmail.com>
2026-01-31 14:51:07 +0700

    Fuctions

---
+++ python6.py
@@ -0,0 +1,23 @@
+"""
+#
+# Fuctions
+#
+"""
+def myFullName(firstName = "Santikan", lastName = "Haobun"):
+    return firstName + " " + lastName
+
+print(myFullName("Lion", "Diet"))
+print(myFullName(""))
+print(myFullName(lastName="Smith"))  
+print(myFullName(firstName="John", lastName="Doe"))  
+print(myFullName())  # using default parameters
+
+def redpotion(hp):
+    return hp + 50
+def bluepotion(mp):
+    return mp + 30
+
+current_hp = 100
+print("Current HP:", current_hp)
+current_hp = redpotion(current_hp)
+print("After red potion, HP:", current_hp)
